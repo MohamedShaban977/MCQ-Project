6f49559c41624456844dc25a6c0979b463125828
MohamedShaban977 <shban977@gmail.com>
2023-10-26 13:46:09 +0300

    adding exam

--- src/app/modules/doctor/components/new-exam/new-exam.component.ts
+++ src/app/modules/doctor/components/new-exam/new-exam.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-exam',
@@ -9,12 +9,38 @@ import { FormBuilder } from '@angular/forms';
 export class NewExamComponent implements OnInit {
 
   currentStep: number = 1;
-  constructor() { }
+
+
+
+  formGroup!: FormGroup;
+  examFG!: FormGroup;
+  constructor(private fb: FormBuilder) { }
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      nameSubject: ['', [Validators.required]],
 
-
+      examList: this.fb.array([''])
+    });
 
   }
+
+  get examList() {
+    return this.formGroup.get('examList') as FormArray
+  }
+
+  addExam() {
+    this.examFG = this.fb.group({
+      answerOne: ['', Validators.required],
+      answerTwo: ['', Validators.required],
+      answerThree: ['', Validators.required],
+      answerFour: ['', Validators.required],
+      correctAnswer: ['', Validators.required]
+    });
+    this.examList.push(this.examFG);
+  }
+
+
+
 
   displayStep(indexStep: number) {
 
